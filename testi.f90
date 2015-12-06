d26213feaf3419773ee1df9a844e559eda6419c4
aajarven <anni.jarvenpaa@gmail.com>
2015-12-06 21:10:44 +0200

    testausta varten, nyt lisäykset ja kierrot

--- testi.f90
+++ testi.f90
@@ -2,6 +2,16 @@
 program testi
     use AVLpuu
     implicit none
-    type (solmu) = juuri
-    juuri = solmu(sana = "a")
+    type (solmu), target :: juuri
+    type (solmu), pointer :: jp
+    type (solmu), target :: lapsi
+    type (solmu), pointer :: lp
+    type (solmu), pointer :: lisatty, lisatty2
+    juuri = solmu(NULL(), NULL(), NULL(), 1, "a")
+    jp => juuri
+    lisatty => lisaa(1, "c", jp, NULL(), jp)
+    lisatty => lisaa(1, "b", jp, NULL(), jp)
+    write(*,*) jp%sana
+    write(*,*) jp%vasen%sana
+    write(*,*) jp%oikea%sana
 end program

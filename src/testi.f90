336468591a050457af80e0b218d582743f5b2ad8
aajarven <anni.jarvenpaa@gmail.com>
2015-12-19 17:50:14 +0200

    uusi tekstitiedosto, jossa vaikeita juttuja

--- src/testi.f90
+++ src/testi.f90
@@ -4,14 +4,19 @@ program testi
     use tiedostonluku
     implicit none
     integer :: i
+    character(len=200) :: sana
+
     !write(*,*) avaa(len("run/simple.txt"), "run/simple.txt")
     !write(*,*) avaa(len("run/kalevala.txt"), "run/kalevala.txt")
-    if (.not. avaa(len("run/shakespear.txt"), "run/shakespear.txt")) then
+    !if (.not. avaa(len("run/shakespear.txt"), "run/shakespear.txt")) then
+    if (.not. avaa(len("run/valimerkit.txt"), "run/valimerkit.txt")) then
         stop
     end if
 
-    do i=1,20
-        write(*,*) lue_sana() 
+    sana = lue_sana()
+    do while (.not. sana .eq. '')
+        write(*,*) trim(sana)
+        sana = lue_sana()
     end do
    
     !type (solmu), target :: juuri

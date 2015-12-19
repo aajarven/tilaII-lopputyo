0ed79645995d5f3ba469c9c7dba7ca9026ffc3c3
aajarven <anni.jarvenpaa@gmail.com>
2015-12-20 01:13:54 +0200

    ?

--- src/testi.f90
+++ src/testi.f90
@@ -1,7 +1,7 @@
 ! tiedostonluvun testausta
 program testi
-    !use AVLpuu
-    use tiedostonluku
+    use AVLpuu
+    !use tiedostonluku
     implicit none
     integer :: i
     character(len=200) :: sana
@@ -9,26 +9,26 @@ program testi
     !write(*,*) avaa(len("run/simple.txt"), "run/simple.txt")
     !write(*,*) avaa(len("run/kalevala.txt"), "run/kalevala.txt")
     !if (.not. avaa(len("run/shakespear.txt"), "run/shakespear.txt")) then
-    if (.not. avaa(len("run/valimerkit.txt"), "run/valimerkit.txt")) then
-        stop
-    end if
+!    if (.not. avaa(len("run/valimerkit.txt"), "run/valimerkit.txt")) then
+!        stop
+!    end if
 
-    sana = lue_sana()
-    do while (.not. sana .eq. '')
-        write(*,*) trim(sana)
-        sana = lue_sana()
-    end do
+!    sana = lue_sana()
+!    do while (.not. sana .eq. '')
+!        write(*,*) trim(sana)
+!        sana = lue_sana()
+!    end do
    
-    !type (solmu), target :: juuri
-    !type (solmu), pointer :: jp
-    !type (solmu), target :: lapsi
-    !type (solmu), pointer :: lp
-    !type (solmu), pointer :: lisatty, lisatty2
-    !juuri = solmu(NULL(), NULL(), NULL(), 1, "a")
-    !jp => juuri
-    !lisatty => lisaa(1, "c", jp, NULL(), jp)
-    !lisatty => lisaa(1, "b", jp, NULL(), jp)
-    !write(*,*) jp%sana
-    !write(*,*) jp%vasen%sana
-    !write(*,*) jp%oikea%sana
+    type (solmu), target :: juuri
+    type (solmu), pointer :: jp
+    type (solmu), target :: lapsi
+    type (solmu), pointer :: lp
+    type (solmu), pointer :: lisatty, lisatty2
+    juuri = solmu(NULL(), NULL(), NULL(), 1, "a")
+    jp => juuri
+    lisatty => lisaa(1, "c", jp, NULL(), jp)
+    lisatty => lisaa(1, "b", jp, NULL(), jp)
+    write(*,*) jp%sana
+    write(*,*) jp%vasen%sana
+    write(*,*) jp%oikea%sana
 end program
